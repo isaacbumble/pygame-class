3fd22ef4e4a390883f7d681902d7d7162286ca20
Isaac <isaacbumble@gmail.com>
2020-04-15 21:20:30 -0700

    homework

--- lesson1/main.py
+++ lesson1/main.py
@@ -1,3 +1,14 @@
-x = 3
-y = (x * 2) + 1
-print(y)
+for i in range(0, 10):
+    spaces = ''
+    for j in range(0, 10 - i - 1):
+        spaces = spaces + ' '
+    stars = ''
+    for j in range(0, i * 2 + 1):
+        stars = stars + '*'
+    print(spaces + stars)
+
+
+
+
+
+
