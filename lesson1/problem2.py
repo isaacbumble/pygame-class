985d46c01f17c302d2480f7f76b9f3b50def971d
Isaac <isaacbumble@gmail.com>
2020-04-19 16:53:18 -0700

    Homework

---
+++ lesson1/problem2.py
@@ -0,0 +1,13 @@
+h = 8
+w = 4
+for i in range(0, h):
+    line = ''
+    if (i == 0) or (i == h - 1):
+        for j in range(0, w):
+          line = line + '*'
+    else:
+        line = '*'
+        for j in range(0, w - 2):
+            line = line + ' '
+        line = line + '*'
+    print(line)
